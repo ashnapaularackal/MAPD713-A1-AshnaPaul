018346378fef6ba5e9d4a6b31867cf6a25085a46
ashnapaularackal <ashnapaularackal@gmail.com>
2024-09-21 19:57:53 -0400

    implement data storage

--- index.js
+++ index.js
@@ -6,29 +6,50 @@ const app  = express();
 app.use(express.json());
 // port number on which you want to run the application
 const port = 5000;
+// 2 request counters, one for GET requests and the other for POST requests
 var getcount =0;
 var postcount =0;
+
+let datastore ={
+    users:[]
+}
 
 app.get('/products',(req,res)=>
 {
     getcount++;
-    res.json({"key":" products GET: received request"})
+    // return a list of all products information as a response to GET request
+    res.json(datastore.users);
+    //log request information
     console.log("products GET: received request")
+    // log request counters values every request:
     console.log(`Processed Request Count--> Get:${getcount}`)
 }
 )
 
 app.post('/products',(req,res)=>{
     postcount++;  
-    const {productId,name,price,quantity}  = req.body;      
-    res.send("done");
+    //  log response information
     console.log("products POST: sending response")
+    // log request counters values every request:
     console.log(`Processed Request Count-->Post:${postcount}`)
+    const userData =req.body;
+    if(!userData || !userData.productId || !userData.name || !userData.price || !userData.quantity){
+
+    return res.status(400).json({error:"Invalid Data"})
+}
+//store in-memory json payload received in POST request 
+datastore.users.push(userData);
+res.status(201).json(userData);
 
 })
 
+
 //port binding
 app.listen(port,()=>{
+
+//     logging the information on start-up:
+// - about the server uri (IP address and port) and endpoints information
+
     console.log(`Server is listening at http://127.0.0.1:5000`);
 })
 
